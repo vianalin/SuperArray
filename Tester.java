7fb2d477bad0ad47bfdb8c051ff929dc7b7f8ad2
vianalin <vlin21@stuy.edu>
2020-11-04 23:27:30 -0500

    tested base methods more thoroughly

--- Tester.java
+++ Tester.java
@@ -5,15 +5,26 @@ public class Tester {
 		
 		System.out.println(a.size()); //testing size
 
-		a.add("hello");
+		a.add("hello"); 
 		a.add("hi");
-		System.out.println(a.size());
+		System.out.println(a.size()); //testing add & size
+
+		a.add("hola");
+		a.add("ni hao");
+		a.add("good morning"); //5
+		a.add("!");
+		a.add("bonjour");
+		a.add("hey");
+		a.add("konnichiwa");
+		a.add("ola");
+		a.add("salut");
+		System.out.println(a.size()); //testing resize
 
 		System.out.println(a.set(1, "what's up")); //hi
 		System.out.println(a.get(1)); //testing set
 
 		//classwork
-		System.out.println(a.isEmpty()); //testing isempty
-		System.out.println(b.isEmpty());
+		System.out.println(a.isEmpty()); //testing isempty false
+		System.out.println(b.isEmpty()); //testing isempty true
 	}
 }
