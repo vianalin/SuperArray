46223ab2cf67a8aaf9bfa10ed6f7b5565f5693ab
vianalin <vlin21@stuy.edu>
2020-11-04 23:28:03 -0500

    fixed resize

--- SuperArray.java
+++ SuperArray.java
@@ -15,12 +15,11 @@ public class SuperArray {
 	}
 
 	public void resize() { //g. increase capacity by creating a larger array & copying values into new one
-		String[] result = new String[size * 2];
+		String[] resizedarr = new String[size * 2]; //+ 10 is too little, * 2 better
 		for(int i = 0; i < size; i++) {
-			result[i] = data[i];
+			resizedarr[i] = data[i];
 		}
-		data = result;
-		size = data.length;
+		data = resizedarr;
 	}
 
 	public boolean add(String element) { //d. appends the specified element to the end of this list
