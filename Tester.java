b7c36bb9ae69499ffcded7b317fd73d6cc34983a
vianalin <vlin21@stuy.edu>
2020-11-07 13:03:16 -0500

    removed classworkand moved it to demo

--- Tester.java
+++ Tester.java
@@ -50,22 +50,6 @@ public class Tester {
 		System.out.println(a.indexOf("ola")); //testing indexof 
 
 		System.out.println(a.toArray()); //testing toarray
-
-		// 11/5 testing classwork
-		SuperArray words = new SuperArray(); 
-    	words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");     
-    	words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    	words.add("una");    words.add("ebi");     words.add("toro"); 
-    	System.out.println(words);
-    	removeDuplicates(words);
-    	System.out.println(words); //testing remove dup
-
 	}
 
-	// 11/5 classwork
-	public static void removeDuplicates(SuperArray s) { //keep first occurence, remove rest of repetition
-		for(int i = s.size() - 1; i > 0; i--) {
-			if(s.indexOf(s.get(i)) != i) s.remove(i); 
-		}
-	}
 }
